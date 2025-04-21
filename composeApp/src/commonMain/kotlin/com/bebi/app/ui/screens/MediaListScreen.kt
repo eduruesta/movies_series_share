6bc3bf4065d0c9a591619d874c26d4dd40b744c7
eduardoruesta <eduruesta@hotmail.com>
2025-04-21 18:12:48 -0300

    feat: Implement dynamic rating and update UI strings
    This commit introduces the following key features and changes:
    
    -   **Dynamic Rating Display:**
        -   The Media List Screen now dynamically displays the average rating and the number of user ratings for each media opinion.
        -   If there is only one rating, it shows the original user's rating.
        -   If there are multiple ratings it shows the average rating with one decimal place.
    -   **Update rating text:**
        - The rating text is now displayed as "X.X (Y)" when there are more than one rating.
    -   **Update Rating Count**:
        - The default `ratingCount` has been updated to `1`.
    -   **UI Enhancements:**
        -   Updated the rating text to "Calificar" in the Media List Screen and Rating Bottom Sheet.
        - Added `rate_action`, `star_description`, `poster_description` string resources.
    -   **Rating Action Label**:
        - Changed the "Calificar" label in the Media List Item to use `stringResource` for localization.
    - **Improve Rating Bottom Sheet**:
        -   Changed the title of the Rating Bottom Sheet to "Calificar" and the button label.
        - Now the button is disabled if not selected rating.

--- composeApp/src/commonMain/kotlin/com/bebi/app/ui/screens/MediaListScreen.kt
+++ composeApp/src/commonMain/kotlin/com/bebi/app/ui/screens/MediaListScreen.kt
@@ -24,7 +24,6 @@ import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.HorizontalDivider
@@ -34,13 +33,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier

f8d643010febbe1afbd7a16a1085e13eeec39266
eduardoruesta <eduruesta@hotmail.com>
2025-04-22 00:08:12 -0300

    feat: Update save opinion logic in MediaOpinionFormViewModel
    This commit modifies the `saveOpinion` logic in `MediaOpinionFormViewModel` to include:
    
    - `ratingCount`: Set to 1.
    - `averageRating`: Set to the selected rating.

--- composeApp/src/commonMain/kotlin/com/bebi/app/viewmodel/MediaOpinionFormViewModel.kt
+++ composeApp/src/commonMain/kotlin/com/bebi/app/viewmodel/MediaOpinionFormViewModel.kt
@@ -153,7 +153,9 @@ class MediaOpinionFormViewModel(
                 rating = rating,
                 comment = comment,
                 synopsis = synopsis,
-                posterUrl = posterUrl
+                posterUrl = posterUrl,
+                ratingCount = 1,
+                averageRating = rating
             )
 
             mediaOpinionRepository.saveOpinion(opinion)

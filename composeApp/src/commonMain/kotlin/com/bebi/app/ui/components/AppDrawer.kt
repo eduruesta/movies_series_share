c615eb897d66928d03c64231e27c3a538c9ef1fd
eduardoruesta <eduruesta@hotmail.com>
2025-04-25 10:21:42 -0300

    feat: Enhance App Drawer UI and add app icon
    This commit enhances the appearance and functionality of the App Drawer in the application.
    
    -   **App Drawer UI Improvements:**
        -   Improved the layout of the `AppDrawerContent` by adding spacing between navigation items and centering them.
        -   Updated the icons color to the `MaterialTheme.colorScheme.primary`.
        -  Add a image of the app icon on the header of the app drawer.
        -  Replaced the old `Text` in the header by the app icon and name.
        - Added more padding to the Column inside the drawer.
        - Added more padding to the NavigationDrawerItems.
        - Updated the imports to use the `Arrangement` and `Alignment`.
    -   **App icon:**
       - Added a new `Image` composable in the header of the app drawer to show the app icon.
    -  - Updated the color of the icons in the NavigationDrawerItem.

--- composeApp/src/commonMain/kotlin/com/bebi/app/ui/components/AppDrawer.kt
+++ composeApp/src/commonMain/kotlin/com/bebi/app/ui/components/AppDrawer.kt
@@ -1,14 +1,18 @@
 package com.bebi.app.ui.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.DrawerState
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -17,17 +21,21 @@ import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.NavigationDrawerItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import moviesseriesshare.composeapp.generated.resources.Res
 import moviesseriesshare.composeapp.generated.resources.app_name
+import moviesseriesshare.composeapp.generated.resources.ic_launcher
 import moviesseriesshare.composeapp.generated.resources.media_list_title
 import moviesseriesshare.composeapp.generated.resources.your_recommendations
+import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 
 /**
  * Drawer content for the application
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AppDrawerContent(
     onNavigateToMediaList: () -> Unit,
@@ -50,18 +58,25 @@ fun AppDrawerContent(
         )
 
         HorizontalDivider()
-        
+
         // Navigation items
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(12.dp)
+                .padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(12.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Spacer(modifier = Modifier.height(12.dp))
-            
+
             // Home
             NavigationDrawerItem(
-                icon = { Icon(Icons.Default.Home, contentDescription = null) },
+                icon = {
+                    Icon(
+                        Icons.Default.Home, contentDescription = null,
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                },
                 label = { Text(stringResource(Res.string.media_list_title)) },
                 selected = false,
                 onClick = {
@@ -69,10 +84,16 @@ fun AppDrawerContent(
                 },
                 modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
             )
-            
+
             // Recommendations
             NavigationDrawerItem(
-                icon = { Icon(bookmarkCheck, contentDescription = null) },
+                icon = {
+                    Icon(
+                        bookmarkCheck,
+                        contentDescription = null,
+                        tint = MaterialTheme.colorScheme.primary
+                    )
+                },
                 label = { Text(stringResource(Res.string.your_recommendations)) },
                 selected = false,
                 onClick = {
@@ -80,32 +101,32 @@ fun AppDrawerContent(
                 },
                 modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
             )
-            
-/*            // Profile
-            NavigationDrawerItem(
-                icon = { Icon(Icons.Default.Person, contentDescription = null) },
-                label = { Text(stringResource(Res.string.profile)) },
-                selected = false,
-                onClick = {
-                    onNavigateToProfile()
-                },
-                modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
-            )
-            
-            Divider(
-                modifier = Modifier.padding(vertical = 12.dp)
-            )
-            
-            // Settings
-            NavigationDrawerItem(
-                icon = { Icon(Icons.Default.Settings, contentDescription = null) },
-                label = { Text(stringResource(Res.string.theme)) },
-                selected = false,
-                onClick = {
-                    onNavigateToSettings()
-                },
-                modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
-            )*/
+
+            /*            // Profile
+                        NavigationDrawerItem(
+                            icon = { Icon(Icons.Default.Person, contentDescription = null) },
+                            label = { Text(stringResource(Res.string.profile)) },
+                            selected = false,
+                            onClick = {
+                                onNavigateToProfile()
+                            },
+                            modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                        )
+
+                        Divider(
+                            modifier = Modifier.padding(vertical = 12.dp)
+                        )
+
+                        // Settings
+                        NavigationDrawerItem(
+                            icon = { Icon(Icons.Default.Settings, contentDescription = null) },
+                            label = { Text(stringResource(Res.string.theme)) },
+                            selected = false,
+                            onClick = {
+                                onNavigateToSettings()
+                            },
+                            modifier = Modifier.padding(NavigationDrawerItemDefaults.ItemPadding)
+                        )*/
         }
     }
 }
